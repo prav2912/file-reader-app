3606e7f8571eaceef4cfba6d25cd9acbecd5723d
prav2912 <mail2prn@yahoo.co.in>
2026-03-10 11:18:42 +0100

    Fix CI/CD workflow and test import for GitHub Actions

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,8 @@
-from app import app
+#from app import app
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from src.app import app
 
 def test_home_page():
     tester = app.test_client()
